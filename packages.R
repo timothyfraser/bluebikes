9b9486a6845c73d8cd52919868e728b28248d97b
Timothy Fraser <timothy.fraser.1@gmail.com>
2024-07-01 13:14:17 -0400

    First big commit

---
+++ packages.R
@@ -0,0 +1,5 @@
+install.packages(c("tidyverse", "sf", "ggspatial", "ggpubr", 
+                   "ggtext", "shadowtext", "fontawesome", "rsvg", "magick",
+                   "Zelig", "texreg", 
+                   "GGally", "tidygraph", "igraph", 
+                   "car", "lmtest", "rgdal"))
